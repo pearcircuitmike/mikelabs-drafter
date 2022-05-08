df8855895eac9a12f66551022513b96d796d4085
Mike Young <mike.young@pearcircuit.com>
2022-05-08 13:52:10 -0400

    correct missing tag close

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -77,7 +77,7 @@ export default function ArticlePage() {
              <meta charSet="utf-8" />
              <title>{article.title}</title>
              <meta name="description" content={article.description}/>
-             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
          </Helmet>
 
 
