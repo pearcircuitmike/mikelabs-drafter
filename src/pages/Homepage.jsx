0f629847dd4c56b076da273841ad32579ed045c0
Mike Young <mike.young@pearcircuit.com>
2022-05-15 09:18:48 -0400

    Updating homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,16 @@
 import {Heading } from '@chakra-ui/react'
 import React from 'react'
 import { Layout } from '../components/Layout'
+import Hero  from '../components/homepage/Hero'
+import GridListWithCTA  from '../components/homepage/GridListWithCTA'
+
 
 export default function Homepage(){
 
   return (
     <Layout>
-      <Heading>Home page TEST2</Heading>
+      <Hero/>
+      <GridListWithCTA/>
     </Layout>
 
 
